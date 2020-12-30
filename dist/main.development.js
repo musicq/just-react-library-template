95da72eedd6c27259514cf822c79030ba1e9aa73
musicq <liang.kui@hotmail.com>
2020-12-30 13:42:07 +0800

    initial commit

---
+++ dist/main.development.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Button(_a) {
+    var onClick = _a.onClick, children = _a.children;
+    return (React.createElement("button", { onClick: onClick }, children));
+}
+
+export { Button };
+//# sourceMappingURL=main.development.js.map
